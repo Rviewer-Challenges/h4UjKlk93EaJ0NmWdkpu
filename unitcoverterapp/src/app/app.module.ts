77c49d8320ea387bb1e372738cbf8cdb949a5481
RicardoIsaac <105662685+RicardoIsaac@users.noreply.github.com>
2023-01-25 19:25:38 -0600

    navbar

--- unitcoverterapp/src/app/app.module.ts
+++ unitcoverterapp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponentsComponent } from './navbar-components/navbar-components.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NavbarComponentsComponent
   ],
   imports: [
     BrowserModule,
